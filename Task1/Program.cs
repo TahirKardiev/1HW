a74bf93f3bc9a121ea3facd8957ca3e7d77f4cc0
Andrew <mr.fesenko@inbox.ru>
2022-08-18 14:36:55 +0300

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] && number[1]==number[3])
+  {
+    Console.WriteLine("Палиндром.");
+  }
+  else 
+  {
+    Console.WriteLine("НЕ палиндром.");
+  }
+}
+
+if (number.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine("Не пятизначное число");
